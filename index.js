19c41769ad8f707aad0fa0e4d9f3708b23225c34
Louis <louissam@hotmail.co.uk>
2025-11-09 20:54:45 +0000

    added javascript driven animation

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const body = document.body;
+const logo = document.getElementById('google-logo');
+
+const maxScroll = document.body.scrollHeight - window.innerHeight;
+window.addEventListener('scroll', () => {
+    const s = window.scrollY / maxScroll;
+    const progress = Math.min(1, Math.max(0, s));
+    logo.style.animationDelay = `-${progress * 2}s`;
+});
 
 let lastTouch
 
